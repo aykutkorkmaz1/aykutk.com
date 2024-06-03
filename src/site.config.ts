7712d70ae1e6e06cbc1020ae1eff3523baf1b97f
Aykut Korkmaz <aykutkorkmaz.original@gmail.com>
2024-06-03 03:50:32 +0300

    site metadata update

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,11 +3,11 @@ import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: 'SRLEOM',
+	author: 'Aykut',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'astro-theme-resume',
+	title: 'Aykuts Website',
 	// Meta property used as the default description meta property
-	description: 'The official Astro Resume Theme',
+	description: 'Official personal website',
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: 'en-GB',
 	// Meta property, found in src/components/BaseHead.astro L:42
